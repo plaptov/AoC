5724f3871c9117d2d6f3199a48b8bcb254f3bf94
Pavel Laptov <pavel.laptov@ya.ru>
2021-12-09 01:29:03 +0300

    2021 day 2

---
+++ 2021/src/day2/mod.rs
@@ -0,0 +1,63 @@
+use crate::reader::read_lines;
+use std::str::FromStr;
+
+enum Move {
+    Forward,
+    Up,
+    Down,
+}
+
+#[derive(Debug)]
+pub struct MoveParseError {}
+
+impl FromStr for Move {
+    type Err = MoveParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "forward" => Ok(Move::Forward),
+            "up" => Ok(Move::Up),
+            "down" => Ok(Move::Down),
+            _ => Err(MoveParseError {}),
+        }
+    }
+}
+
+pub fn day2_fun() {
+    let bytes: &[u8] = include_bytes!("input.txt");
+    let moves: Vec<(Move, i32)> = read_lines(bytes)
+        .iter()
+        .map(|s| {
+            let (s1, s2) = s.split_once(&" ").unwrap();
+            (s1.parse().unwrap(), s2.parse().unwrap())
+        })
+        .collect();
+
+    let mut horizontal_pos = 0;
+    let mut depth = 0;
+    for (m, i) in moves.iter() {
+        match m {
+            Move::Forward => horizontal_pos += i,
+            Move::Up => depth -= i,
+            Move::Down => depth += i,
+        }
+    }
+
+    println!("Product: {}", horizontal_pos * depth);
+
+    horizontal_pos = 0;
+    depth = 0;
+    let mut aim = 0;
+    for (m, i) in moves.iter() {
+        match m {
+            Move::Forward => {
+                horizontal_pos += i;
+                depth += aim * i;
+            }
+            Move::Up => aim -= i,
+            Move::Down => aim += i,
+        }
+    }
+
+    println!("Product 2: {}", horizontal_pos * depth);
+}
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,7 +1,9 @@
 #[warn(clippy::all)]
 mod day1;
+mod day2;
 mod reader;
 
 fn main() {
     day1::day1_fun();
+    day2::day2_fun();
 }
